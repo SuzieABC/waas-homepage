d41ca29dfa8ab70f6514e3e281689fb10f344308
SuzieABC <suzie@ahnlab.io>
2024-12-09 16:47:03 +0900

    feat: set initial structure

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@/styles/globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="max-w-[1440px] mx-auto border">{children}</main>
+      <Footer />
+    </>
+  );
+}
