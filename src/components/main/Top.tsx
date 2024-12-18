680d905fa32f874008618b7ead77a453a98fea48
SuzieABC <suzie@ahnlab.io>
2024-12-18 17:53:47 +0900

    chore: add willchange in css to prevent thin line between images

--- src/components/main/Top.tsx
+++ src/components/main/Top.tsx
@@ -1,60 +1,165 @@
 "use client";
 
 import Image from "next/image";
-import blue_head from "@/assets/images/Vector_blue_head.png";
-import blue_tail from "@/assets/images/Vector_blue_tail.png";
+import blue_head from "@/assets/images/top/blue_card_head_image.png";
+import blue_tail from "@/assets/images/top/blue_card_tail_image.png";
 import useWindowWidth from "@/utils/localization/hooks/useWindowWidth";
+import sphere from "@/assets/images/top/sphere_image.png";
+import star from "@/assets/images/top/star_image.png";
+import curve from "@/assets/images/top/curve_image.png";
+import line from "@/assets/images/top/line_image.png";
+import sphereLine from "@/assets/images/top/sphere_line_image.png";
 
-interface TopProps {
-  title: string;
-  locale: string;
-}
-// export default function Top({ title, locale }: TopProps) {
-//   return (
-//     <div className="w-full h-lvh relative flex items-center justify-center">
-//       {/* 이미지 배경 */}
-//       <div className="relative w-full px-[40px]">
-//         <div className="flex w-full h-full items-end">
-//           <Image src={blue_head} alt="head" height={810} />
-//           <div className="w-full h-[747px] bg-[#3140F1] mt-[63px]"></div>
-//           <Image src={blue_tail} alt="tail" height={810} />
-//         </div>
-//         {/* 텍스트와 버튼 */}
-//         <div className="absolute top-[140px]">
-//           <p className="text-[100px] pl-[160px]">{title}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+import blue_card_head_360 from "@/assets/images/top/360_blue_card_head.png";
+import blue_card_tail_360 from "@/assets/images/top/360_blue_card_tail.png";
+import { useTranslation } from "@/utils/localization/client";
+import { useParams } from "next/navigation";
+import type { LocaleTypes } from "@/utils/localization/settings";
+import { useState, useEffect } from "react";
 
-export default function Top({ title, locale }: TopProps) {
-  const { windowWidth, isMobile, isTablet, isDesktop, isLargeDesktop } =
-    useWindowWidth();
+export default function Top() {
+  const locale = useParams()?.locale as LocaleTypes;
+  const { t } = useTranslation(locale, "main");
+  const { isMobile, isTablet, isDesktop, isLargeDesktop } = useWindowWidth();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsLoading(true), 200);
+
+    window.history.scrollRestoration = "manual";
+    window.scrollTo(0, 0);
+
+    return () => {
+      clearTimeout(timeout);
+      window.history.scrollRestoration = "auto";
+    };
+  }, []);
+
   return (
-    <div className="w-full h-lvh relative flex items-center justify-center">
-      {/* 이미지 배경 */}
-      <div className="relative w-full px-[40px]">
-        <div className="flex w-full h-full items-end">
+    <div
+      className={`w-full h-lvh relative whitespace-pre overflow-hidden flex items-center justify-center ${
+        isMobile || isTablet ? "pt-[60px]" : "pt-[80px]"
+      }`}
+    >
+      {isLoading && (
+        <div
+          className={`w-full relative !will-change-transform ${
+            isMobile
+              ? "px-[12px] mt-[40px] mb-[40px]"
+              : isTablet
+              ? "px-[20px]"
+              : "px-[40px]"
+          }`}
+        >
+          {(isMobile || isTablet) && (
+            <div className="flex w-full items-end">
+              <Image
+                src={blue_card_head_360}
+                alt="mobile_head"
+                height={618}
+                priority
+              />
+              <div className="w-full h-[572px] bg-[#3140F1]" />
+              <Image
+                src={blue_card_tail_360}
+                alt="mobile_tail"
+                height={618}
+                priority
+              />
+            </div>
+          )}
+
+          {(isDesktop || isLargeDesktop) && (
+            <div className="flex w-full items-end">
+              <Image src={blue_head} alt="head" height={810} priority />
+              <div className="w-full h-[747px] bg-[#3140F1] mt-[63px]"></div>
+              <Image src={blue_tail} alt="tail" height={810} priority />
+            </div>
+          )}
+
+          {!isMobile && (
+            <Image
+              src={line}
+              alt="line shape"
+              className="absolute bottom-0 right-0"
+              width={isTablet ? 322 : 454.03}
+            />
+          )}
+          {!isMobile && (
+            <Image
+              src={sphere}
+              alt="sphere shape"
+              className={`animate-float absolute ${
+                isTablet
+                  ? "top-[264px] right-[-40px]"
+                  : "top-[207px] right-[46px]"
+              }`}
+              width={isTablet ? 360 : 508}
+            />
+          )}
+
+          {isMobile && (
+            <Image
+              src={sphereLine}
+              alt="line shape"
+              width={245}
+              className="animate-float absolute top-[264px] right-[-50px]"
+            />
+          )}
           <Image
-            src={blue_head}
-            alt="head"
-            height={810}
-            className={`${(isMobile || isTablet) && "min-w-[90px]"} h-[810px]`}
+            src={star}
+            alt="star shape"
+            className={`animate-float absolute ${
+              isMobile
+                ? "left-[42px] top-[462.62px]"
+                : isTablet
+                ? "left-[49px] top-[445px]"
+                : "left-[144px] top-[494px]"
+            }`}
+            width={isMobile ? 110.77 : isTablet ? 130 : 246}
           />
-          <div className="w-full h-[747px] bg-[red] mt-[63px]"></div>
           <Image
-            src={blue_tail}
-            alt="tail"
-            height={810}
-            className={`${(isMobile || isTablet) && "min-w-[177px]"} h-[810px]`}
+            src={curve}
+            alt="curve shape"
+            className={`animate-float absolute top-[14px]  ${
+              isMobile
+                ? "left-[100px]"
+                : isTablet
+                ? "left-[179px]"
+                : "left-[394px]"
+            }`}
+            width={isMobile ? 68.61 : isTablet ? 78 : 121}
           />
+          <div
+            className={`absolute ${
+              isMobile
+                ? "top-[98px] left-[40px]"
+                : isTablet
+                ? "top-[100px] left-[80px]"
+                : isDesktop
+                ? "top-[140px] left-[134px]"
+                : "top-[140px] left-[200px]"
+            }`}
+          >
+            <p
+              className={`font-dmSansSemibold ${
+                isMobile
+                  ? "text-[56px] leading-[67.2px]"
+                  : isTablet
+                  ? "text-[66px] leading-[83.16px]"
+                  : "text-[100px] leading-[120px]"
+              }`}
+            >
+              {t("top.title_1")}
+              <br />
+              {t("top.title_2")}&nbsp;
+              {isMobile && <br />}
+              {t("top.title_3")}
+            </p>
+          </div>
         </div>
-        {/* 텍스트와 버튼 */}
-        <div className="absolute top-[140px]">
-          {/* <p className="text-[100px] pl-[160px]">{title}</p> */}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/main/WhyABCWaaS.tsx
+++ src/components/main/WhyABCWaaS.tsx
@@ -1,33 +1,131 @@
 "use client";
 
-import head from "@/assets/images/Vector_head.png";
+import head from "@/assets/images/white_card_head.png";
 import Image from "next/image";
-
 import card2Data from "@/data/why_abc_waas_2.json";
 import { useParams } from "next/navigation";
 import { useTranslation } from "@/utils/localization/client";
 import type { LocaleTypes } from "@/utils/localization/settings";
+import useWindowWidth from "@/utils/localization/hooks/useWindowWidth";
+import pic1 from "@/assets/images/whyAbcWaas/pic1.png";
+import white_card_head_360 from "@/assets/images/whyAbcWaas/360_white_card_head.png";
+import white_card_tail_360 from "@/assets/images/whyAbcWaas/360_white_card_tail.png";
+import { useState, useEffect } from "react";
 
 export default function WhyABCWaaS() {
   const locale = useParams()?.locale as LocaleTypes;
   const { t } = useTranslation(locale, "main");
+  const { isMobile, isTablet, isDesktop, isLargeDesktop } = useWindowWidth();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsLoading(true), 200);
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
-    <div className="pb-[240px]">
-      {/* <CardWrapper data={card1Data} title={t("why_abc_waas.title")} /> */}
-      <div className="bg-[#3140F1] w-full py-[140px] px-[38px] rounded-[100px] text-black">
-        {card2Data.map((data) => (
-          <div key={data.id} className="flex w-full h-full py-[60px]">
-            <Image src={head} alt="head" height={620} />
-            <div className="w-full h-[620px] bg-[#F0F1F4] rounded-tr-[50px] rounded-br-[50px] p-6">
-              <p className="text-xl font-semibold">{t(data.title)}</p>
-              <span className="text-sm text-gray-700">
-                {t(data.description)}
-              </span>
+    <div
+      className={`${
+        isMobile
+          ? "mt-[80px]"
+          : isTablet
+          ? "mt-[100px]"
+          : isDesktop
+          ? "mt-[100px]"
+          : "mt-[240px]"
+      }`}
+    >
+      {isLoading && (
+        <div
+          className={`bg-[#3140F1] w-full text-black grid ${
+            isMobile
+              ? "rounded-[28px] pt-[80px] pb-[124px] px-[20px] grid-cols-1 gap-[60px]"
+              : isTablet
+              ? "rounded-[40px] py-[100px] px-[20px] grid-cols-2 gap-[16px]"
+              : isDesktop
+              ? "rounded-[80px] py-[200px] px-[40px] grid-cols-1 gap-[120px]"
+              : "rounded-[80px] py-[200px] px-[40px] grid-cols-1 gap-[120px]"
+          }`}
+        >
+          {card2Data.map((data, index) => (
+            <div
+              key={data.id}
+              className={`flex w-full h-full relative !will-change-transform ${
+                isTablet && (index === 0 || index === 1) && "pb-[24px]"
+              }`}
+            >
+              <Image
+                src={isMobile || isTablet ? white_card_head_360 : head}
+                alt="head of the card"
+                height={isMobile || isTablet ? 576 : 620}
+              />
+              <div
+                className={`w-full bg-[#F0F1F4] ${
+                  isMobile || isTablet
+                    ? "h-[534px]"
+                    : "h-[620px] rounded-tr-[50px] rounded-br-[50px]"
+                }`}
+              />
+              <Image
+                src={
+                  isMobile || isTablet
+                    ? white_card_tail_360
+                    : white_card_tail_360
+                }
+                alt="tail of the card"
+                height={isMobile || isTablet ? 576 : 620}
+                className={`${(isDesktop || isLargeDesktop) && "hidden"}`}
+              />
+
+              <div
+                className={`absolute ${
+                  isMobile || isTablet
+                    ? "top-[40px] left-[19px]"
+                    : isDesktop
+                    ? "top-[80px] left-[80px]"
+                    : "top-[80px] left-[120px]"
+                }`}
+              >
+                <p
+                  className={`${
+                    locale === "ko" ? "font-pretendardBold" : "font-dmSansBold"
+                  } whitespace-pre-wrap ${
+                    isMobile || isTablet
+                      ? "text-[34px] leading-[44.2px] pb-[16px]"
+                      : "text-[64px] leading-[74.24px] pb-[24px]"
+                  }`}
+                >
+                  {t(data.title)}
+                </p>
+                <span
+                  className={`whitespace-pre-wrap ${
+                    isMobile || isTablet ? "text-[16px]" : "text-[24px]"
+                  } ${
+                    locale === "ko"
+                      ? "font-pretendardMedium"
+                      : "font-dmSansMedium"
+                  }`}
+                >
+                  {t(data.description)}
+                </span>
+              </div>
+              <Image
+                src={pic1}
+                alt="image"
+                width={isMobile || isTablet ? 257.15 : 420}
+                className={`absolute ${
+                  isMobile || isTablet
+                    ? "top-[236px] left-1/2 transform -translate-x-1/2"
+                    : isDesktop
+                    ? "top-[114px] right-[80px]"
+                    : "top-[114px] right-[120.1px]"
+                } `}
+              />
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
