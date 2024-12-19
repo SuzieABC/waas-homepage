aa57f19a3542d1e82683111c6eae033ab5258187
SuzieABC <suzie@ahnlab.io>
2024-12-19 15:37:25 +0900

    chore: remove commented lines

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,13 +29,6 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
       fontFamily: Object.fromEntries(fonts.map((font) => [font, [`var(--font-${font})`]])),
-      // screens: {
-      //   '2xl': '1920px', // 1920px 이상
-      //   'xl': '1440px',  // 1440px 이상
-      //   'lg': '1024px',  // 1024px 이상
-      //   'md': '700px',   // 700px 이상
-      //   'sm': '360px',   // 360px 이상
-      // },
       screens: {
         // 최대 너비로 범위 설정
         sm: { min: "0px", max: "699px" }, // 0 ~ 360px
