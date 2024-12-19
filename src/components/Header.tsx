f3a7fff8d8c67db7dc634e5239401cbca023290c
SuzieABC <suzie@ahnlab.io>
2024-12-19 13:15:43 +0900

    chore: add visible effect on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,21 +6,48 @@ import { useParams } from "next/navigation";
 import Image from "next/image";
 import ChangeLocale from "./ChangeLocale";
 import useWindowWidth from "@/utils/localization/hooks/useWindowWidth";
+import useEventListener from "@/utils/localization/hooks/useEventListener";
 import logo from "@/assets/images/header_logo_image.png";
 import { useState, useEffect } from "react";
 
 export default function Header() {
   const { isMobile, isTablet } = useWindowWidth();
   const locale = useParams()?.locale as LocaleTypes;
+  const [isScrolled, setIsScrolled] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
     const timeout = setTimeout(() => setIsLoading(true), 200);
     return () => clearTimeout(timeout);
   }, []);
 
+  const handleScrollBackground = () => {
+    setIsScrolled(window.scrollY > 10);
+  };
+
+  const handleScollNav = () => {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > lastScrollY) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+    setLastScrollY(currentScrollY);
+  };
+
+  useEventListener("scroll", handleScrollBackground);
+  useEventListener("scroll", handleScollNav);
+
   return (
-    <div className="flex justify-center fixed w-full z-[9999]">
+    <div
+      className={`flex justify-center fixed w-full z-[9999] ${
+        isScrolled ? "bg-blue-700" : "bg-transparent"
+      } transition-transform duration-500 ${
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       {isLoading && (
         <header
           className={`flex justify-between items-center max-w-[1920px] w-full mx-auto px-[40px] ${
